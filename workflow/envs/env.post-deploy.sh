87c88de6613cd1764f9e7a0c8616ef034744edb8
Kelly Sovacool <kellysovacool@gmail.com>
2023-06-08 16:11:21 -0400

    Use main branch of sovatools

--- workflow/envs/env.post-deploy.sh
+++ workflow/envs/env.post-deploy.sh
@@ -1,5 +1,4 @@
 #!env bash
-R -e "devtools::install_github('kelly-sovacool/sovatools',
-                               branch = 'quarto-helpers'
+R -e "devtools::install_github('kelly-sovacool/sovatools'
                                )
 "
